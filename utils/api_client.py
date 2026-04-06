0fa99ad97d3c6194d3383f28076148916e67a2be
sai <gannu@gmail.com>
2026-04-06 22:42:25 +0530

    Fixed 403 issue by adding headers

--- utils/api_client.py
+++ utils/api_client.py
@@ -1,19 +1,13 @@
 import requests
-from config.config import BASE_URL, TIMEOUT
-from utils.logger import get_logger
+from config.config import BASE_URL
 
-logger = get_logger()
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "Content-Type": "application/json"
+}
 
 def get(endpoint):
-    url = f"{BASE_URL}{endpoint}"
-    logger.info(f"GET Request: {url}")
-    response = requests.get(url, timeout=TIMEOUT)
-    logger.info(f"Response Status: {response.status_code}")
-    return response
+    return requests.get(BASE_URL + endpoint, headers=headers)
 
 def post(endpoint, payload):
-    url = f"{BASE_URL}{endpoint}"
-    logger.info(f"POST Request: {url} | Payload: {payload}")
-    response = requests.post(url, json=payload, timeout=TIMEOUT)
-    logger.info(f"Response Status: {response.status_code}")
-    return response
+    return requests.post(BASE_URL + endpoint, json=payload, headers=headers)
